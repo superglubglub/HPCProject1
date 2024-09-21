ccef6e3a365f6d93954c4bd85ffd18345dc1b657
superglubglub <maxyslater@pm.me>
2024-09-21 20:06:57 +0800

    edited naive multi

--- matrices.c
+++ matrices.c
@@ -65,11 +65,12 @@ int** multiplyMatrix(int **matrix_1, int **matrix_2) {
         result[i] = calloc(DEFAULT_SIZE, sizeof(int));
     } printf("\t\tAllocated %lu bytes for matrix...\n", DEFAULT_SIZE * sizeof(int) * DEFAULT_SIZE);
 
-    for (int i = 0; i < DEFAULT_SIZE; i++) {
-        for (int j = 0; j < DEFAULT_SIZE; j++) {
-            int tmp = 0;
-            #pragma omp parallel for reduction(+:tmp)
-            for (int k = 0; k < DEFAULT_SIZE; k++) {
+    int i, j, k, tmp;
+    #pragma omp parallel for private(i, j, k) reduction(+:tmp)
+    for (i = 0; i < DEFAULT_SIZE; i++) {
+        for (j = 0; j < DEFAULT_SIZE; j++) {
+            tmp = 0;
+            for (k = 0; k < DEFAULT_SIZE; k++) {
                  tmp += matrix_1[i][k] * matrix_2[k][j];
                 //printf("%2d",omp_get_thread_num());
             }
