0e93bd02c173aac47c9e9c9ab05e8c10399df385
superglubglub <maxyslater@pm.me>
2024-09-21 17:28:23 +0800

    made stats a reference

--- logs.c
+++ logs.c
@@ -35,12 +35,11 @@ int closeLogs(FILE *fp, STATS stats) {
 }
 
 int writeOperation(FILE *fp, char* funcName, STATS* stats) {
-    double runtime = getRuntime(stats->runtime, omp_get_wtime());
+    stats->runtime = getRuntime(stats->runtime, omp_get_wtime());
     fprintf(fp,
         "\tPerformed %s in %.6fs\n",
-        funcName, runtime);
+        funcName, stats->runtime);
     fflush(fp);
-    stats->runtime = runtime;
     return 0;
 }
 
