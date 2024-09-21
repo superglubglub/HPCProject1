11484b77573ecb68b599bd25ee0591a6ea25fd7e
superglubglub <maxyslater@pm.me>
2024-09-21 20:40:07 +0800

    maybe fixed collapse statement

--- matrices.c
+++ matrices.c
@@ -14,7 +14,7 @@ int** createMatrix(float prob)
     } printf("\t\tAllocated %lu bytes for new matrix...\n", DEFAULT_SIZE * DEFAULT_SIZE * sizeof(int));
 
     int i, j;
-    #pragma omp parallel for private(i, j) collapse(2)
+    #pragma omp parallel for private(j) collapse(2)
     for (i = 0; i < DEFAULT_SIZE; i++)
     {
         for (j = 0; j < DEFAULT_SIZE; j++)
