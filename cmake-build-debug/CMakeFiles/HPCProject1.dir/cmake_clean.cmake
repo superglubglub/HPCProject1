d9036b6b26055b876a52ac4129fed9155bb1fb26
superglubglub <maxyslater@pm.me>
2024-09-21 18:10:21 +0800

    idk

---
+++ cmake-build-debug/CMakeFiles/HPCProject1.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HPCProject1.dir/logs.c.o"
+  "CMakeFiles/HPCProject1.dir/logs.c.o.d"
+  "CMakeFiles/HPCProject1.dir/main.c.o"
+  "CMakeFiles/HPCProject1.dir/main.c.o.d"
+  "CMakeFiles/HPCProject1.dir/matrices.c.o"
+  "CMakeFiles/HPCProject1.dir/matrices.c.o.d"
+  "CMakeFiles/HPCProject1.dir/sparsematrix.c.o"
+  "CMakeFiles/HPCProject1.dir/sparsematrix.c.o.d"
+  "HPCProject1"
+  "HPCProject1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HPCProject1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/HPCProject1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for HPCProject1.
+# This may be replaced when dependencies are built.
