07eb3b02dcc5709144552e2bcb29f2a31ef3cad1
superglubglub <maxyslater@pm.me>
2024-09-22 18:13:50 +0800

    int limit reached

--- vars.h
+++ vars.h
@@ -1,5 +1,5 @@
 #define DEFAULT_PROBABILITIES (float[]){0.01, 0.02, 0.05}
-#define DEFAULT_SIZE 10000
+#define DEFAULT_SIZE 100000L
 #define NUM_THREADS 128
 #define LIMIT 10
 #define RAND_RANGE 100
